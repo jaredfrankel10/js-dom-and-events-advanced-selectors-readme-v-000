de40ae9b312f6e8f8987cffbf8fb24029ae1fb21
jared frankel <jaredfrankel10@gmail.com>
2018-02-02 14:43:22 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -2,19 +2,19 @@
 function paragraphSelector() {
    return $('p')
  };
- 
+
  function lastImageSelector() {
    return $('img:last')
  }
- 
+
  function ninjaBabySelector() {
    return $('#baby-ninja')
  }
- 
+
  function divSelector() {
    return $('.pics')
  }
- 
+
  function firstListItem() {
    return $('ul li:first-child')
  }
